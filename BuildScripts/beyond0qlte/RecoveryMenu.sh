392bc37b4d450355f68ba1910da3507856b4336c
PizzaG <49663808+PizzaG@users.noreply.github.com>
2021-05-06 21:06:59 -0500

    Update RecoveryMenu.sh

--- BuildScripts/beyond0qlte/RecoveryMenu.sh
+++ BuildScripts/beyond0qlte/RecoveryMenu.sh
@@ -3,12 +3,6 @@
 # Copyright (C) 2016 BeansTown106 for PureNexus Project
 # Copyright (C) 2019-Present A-Team Digital Solutions
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
 
 clear
 
@@ -87,7 +81,7 @@ recovery8() {
 
 upload() {
   # Upload To Android File Host
-  proxychains lftp -e "mirror -R /media/pizzag/Android/Uploads/Recoveries /Recoveries; bye" ftp://PizzaG:NhymsgQoCrcl@uploads.androidfilehost.com:21
+  proxychains lftp -e "mirror -R /media/pizzag/Android/Uploads/Recoveries /Recoveries; bye" ftp://YourUserName:YourPassword@uploads.androidfilehost.com:21
 }
 
 # ----------------------------------------------------------
